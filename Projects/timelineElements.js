bc535d89803f79c64b3cf4414735ce9da8715b41
kesava <katammagarikesava@gmail.com>
2024-12-30 22:46:01 +0530

    Resolved merge conflicts

--- Projects/timelineElements.js
+++ Projects/timelineElements.js
@@ -1,22 +1,23 @@
 let timelineElements = [
   {
     id: 1,
-    title: "Frontend Developer",
-    location: "Dragontail, Ascana",
+    title: "Nxt Tredz",
+    location: "Frontend : HTML, CSS, Javasrcipt, react js ",
+    locatio: "Rest Api's ",
     description:
-      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+      "Developed by using REST API’s, React js, HTML, CSS, and Bootstrap Nxt Trendz is a dynamic ecommerce platform designed to cater to the diverse shopping needs of modern consumers products including clothing, electronics, toys, grocery items, and appliances",
     buttonText: "View Frontend Projects",
-    date: "August 2016 - present",
+    date: "Nxt Tredz",
     icon: "work",
   },
   {
     id: 2,
-    title: "Backend Developer",
+    title: "Online Event Booking Platformr",
     location: "Skystead, Craonia",
     description:
-      "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+      "Technologies I used were HTML, CSS, Bootstrap, react .js, SQL, and Node.js. The primary objective of our website is to provide a comprehensive and user-friendly platform for event management and minimize manual efforts for ticket booking..",
     buttonText: "View Backend Projects",
-    date: "June 2013 - August 2016",
+    date: "Online Event Booking Platform",
     icon: "work",
   },
   {
