354ae71a8192779b3fccc5693ac45ac952281c5b
kesava <katammagarikesava@gmail.com>
2025-02-15 23:40:34 +0530

    Resolved merge conflicts

--- About/index.js
+++ About/index.js
@@ -2,30 +2,41 @@ import Navbar from '../Navbar';
 import './index.css';
 
 const About = () => {
-    return (
-        <>
-            <Navbar />
-            <div className='bg'>
-                <h1 className='head'>About</h1>
-                <div className='divide'>
-                    <div className='left'>
-                        <p>
-                            I’m Kesava Katammagari, I'm currently working as a QA Engineer at Ramco Systems, specializing in the Payce payroll solutions and RVW
-                            Working on Payce,payroll management product,focusing on delivering high-quality payroll solutions
-                     Designed and executed testcases for payroll processing modules, ensuring compliance with statutory regulations and organizational requirements
-                           <br/> Beyond my QA expertise, I have a strong passion for front-end and web development. Over the years, I have gained extensive experience building websites,
-                             showcasing my skills in crafting engaging, user-friendly, and responsive designs,
-                            I’ve gained my experience working on Ecommerce projects in my academics
-                            When not working on a project or thinking about programming, I'm most likely watching movies, playing cricket, sight-seeing, spending time with family and friends.
-                        </p>
-                    </div>
-                    <div className='right'>
-                        <img src="./kesava.jpg" alt="Kesava Katammagari" className='image' />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <div className="bg">
+        <h1 className="head">About</h1>
+        <div className="divide">
+          <div className="left">
+    
+            <p className='para'>
+              I’m Kesava Katammagari, I'm currently working as a QA Engineer at Ramco Systems, specializing in the Payce payroll solutions and RVW. 
+              Working on Payce, a payroll management product, focusing on delivering high-quality payroll solutions. 
+              I designed and executed test cases for payroll processing modules, ensuring compliance with statutory regulations and organizational requirements.
+              <br />
+              <ul>
+                <li>
+                Beyond my QA expertise, I have a strong passion for <span className="spaan"> Frontend & Web Development.</span>
+              Over the years, I have gained extensive experience building websites,
+              showcasing my skills in crafting engaging, user-friendly, and responsive designs. 
+              I’ve gained my experience working on eCommerce projects in my academics.
+                </li>
+                <li>
+                When not working on a project or thinking about programming, I'm most likely watching movies, playing cricket, sight-seeing, or spending time with family and friends.
+                </li>
+
+              </ul>
+            
+            </p>
+          </div>
+          <div className="right">
+            <img src="./kesava.jpg" alt="Kesava Katammagari" className="image" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default About;
