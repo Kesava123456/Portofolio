a68dfbfc68d0cc346e2c4e5aa68c84ae868e2d98
kesava <katammagarikesava@gmail.com>
2025-02-17 00:00:43 +0530

    Resolved merge conflicts

--- About/index.js
+++ About/index.js
@@ -13,14 +13,17 @@ const About = () => {
             <p className='para'>
               I’m Kesava Katammagari, I'm currently working as a QA Engineer at Ramco Systems, specializing in the Payce payroll solutions and RVW. 
               Working on Payce, a payroll management product, focusing on delivering high-quality payroll solutions. 
-              I designed and executed test cases for payroll processing modules, ensuring compliance with statutory regulations and organizational requirements.
               <br />
               <ul>
                 <li>
                 Beyond my QA expertise, I have a strong passion for <span className="spaan"> Frontend & Web Development.</span>
-              Over the years, I have gained extensive experience building websites,
-              showcasing my skills in crafting engaging, user-friendly, and responsive designs. 
-              I’ve gained my experience working on eCommerce projects in my academics.
+              Over the years in acadamedics, HTML5, CSS3, JavaScript, React, Redux, Java, NodeJS, and Bootstrap. Adept in web application development using Spring,SOAP, RESTful APIs, and GIT, Github. Proficient in React, utilizing features like components, lifecycle methods, and state management using Flux and Redux architectures. Experienced in all phases of SDLC and Agile methodologies. 
+              
+                </li>
+                <li>
+                I have gained extensive experience building websites,
+                showcasing my skills in crafting engaging, user-friendly, and responsive designs. 
+                I’ve gained my experience working on eCommerce projects in my academics.
                 </li>
                 <li>
                 When not working on a project or thinking about programming, I'm most likely watching movies, playing cricket, sight-seeing, or spending time with family and friends.
