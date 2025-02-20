5f14daa4a561a9682345138ff9871ba9f6a964ed
kesava <katammagarikesava@gmail.com>
2025-02-20 22:33:44 +0530

    Resolved merge conflicts

--- Projects/timelineElements.js
+++ Projects/timelineElements.js
@@ -1,18 +1,18 @@
 let timelineElements = [
   {
     id: 1,
-    title: "Nxt Tredz",
-    location: "Tech stack : HTML, CSS, Javasrcipt, react js, REST Api's, Intigration api's",
+    title: "Nxt Trendz",
+    location: "Tech stack :Javasrcipt, React js, REST Api's, Intigration api's, HTML, CSS",
     description:
-      "Developed by using REST API’s, React js, HTML, CSS, and Bootstrap Nxt Trendz is a dynamic ecommerce platform designed to cater to the diverse shopping needs of modern consumers products including clothing, electronics, toys, grocery items, and appliances",
+      "Nxt Trendz is a dynamic ecommerce platform designed to cater to the diverse shopping needs of modern consumers products including clothing, electronics, toys, grocery items, and appliances",
     buttonText: "View Frontend Projects",
     date: "Nxt Tredz",
     icon: "work",
   },
   {
     id: 2,
-    title: "Online Event Booking Platformr",
-    location: "Tech stack : HTML, CSS, Javasrcipt, react js, REST Api's, Intigration api's",
+    title: "Online Event Booking Platform",
+    location: "Tech stack : HTML, CSS, Javasrcipt, react js, REST Api's, SQL, PHP",
     description:
       "Technologies I used were HTML, CSS, Bootstrap, react .js, SQL, and Node.js. The primary objective of our website is to provide a comprehensive and user-friendly platform for event management and minimize manual efforts for ticket booking..",
     buttonText: "View Backend Projects",
@@ -21,10 +21,10 @@ let timelineElements = [
   },
   {
     id: 3,
-    title: "E-commerce Website",
-    location: "Tech stack : HTML, CSS, Javasrcipt, react js, REST Api's, Intigration api's",
+    title: "E-commerce Website", 
+    location: "Tech stack : HTML, CSS, Bootstrap, Javasrcipt, SQL",
     description:
-      "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
+      "This is Clothing E-commerce Webiste developed by web technologies, providing lot of Mens fahions,and users can easily order.",
     buttonText: "Company Website",
     date: "E-commerce Website",
     icon: "work",
