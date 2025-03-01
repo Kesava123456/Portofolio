0a00ae1eee93549e44144371d77c87bfb27eae89
kesava katammagari <117719799+Kesava123456@users.noreply.github.com>
2025-03-01 19:46:55 +0530

    Delete src/Component directory

--- src/Component/CustomeHook/DocTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useDocTitle = (count) => {
-  useEffect(() => {
-    document.title = `count ${count}`;
-  }, [count]); // Correctly call useEffect with a function and dependency array
-};
-
-export default useDocTitle;
--- src/Component/UseContext/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserContex =()=>{
-
-    return(
-        <div>Theme Swicher</div>
-    )
-}
-export default UserContex
